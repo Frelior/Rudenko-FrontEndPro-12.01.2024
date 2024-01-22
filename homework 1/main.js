f44da4674806a0fc3f7660537976ac93f5466473
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-01-22 15:24:04 +0200

    create a folder for hw1

--- homework 1/main.js
+++ homework 1/main.js
@@ -2,6 +2,7 @@ function Hello() {
     const userName = prompt("What is your name?") || 'mr. incognito'; ;
     alert(`Hello, ${userName}! How are you?`);
 }
+
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', Hello);
 Hello();
