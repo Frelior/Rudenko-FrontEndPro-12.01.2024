77f733a516ea679f49182c6a3debb0e10d436846
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-02-01 14:22:57 +0200

    hotfix

--- lesson4/4.2/main.js
+++ lesson4/4.2/main.js
@@ -23,7 +23,7 @@ function isPrime(number) {
     if (number <= 1) {
         return false;
     }
-    for (let i = 2; i <= Math.sqrt(number); i++) {
+    for (let i = 2; i <= number; i++) {
         if (number % i === 0) {
             return false;
         }
