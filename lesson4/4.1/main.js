589dedd795c6715cd3fd2956785031396ee196ae
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-02-01 12:25:38 +0200

    made homework7

---
+++ lesson4/4.1/main.js
@@ -0,0 +1,5 @@
+const num1 = +prompt('input first number') || 0;
+const num2 = +prompt('input second number') || 0;
+const num3 = +prompt('input third number') || 0;
+const mean = (num1 + num2 + num3) / 3;
+alert('The arithmetic mean is: ' + mean);
