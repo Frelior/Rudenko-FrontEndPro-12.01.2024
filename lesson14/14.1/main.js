8272090896cbd1db3aa6b65c02f9a5925966cadf
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-03-12 15:54:54 +0200

    made counter

--- lesson14/14.1/main.js
+++ lesson14/14.1/main.js
@@ -1,17 +1,19 @@
-const array = [1, 2, 3, 4, 5, 6, [1, 2, 3, 4], 7, 8, 9]
+const array = [1, 222, 3, 44, 5, 6, [1, 2, 3, 4], 7, 8, 9, [1, 2, 3], 10, 11]
 const body = document.querySelector('body')
 
-function generateList(domElement, array) {
+function generateList(domElement, array, prefix = null) {
     const newList = document.createElement('ul')
     const newItems = []
+    let callCounter = 0
 
     array.forEach(element => {
         const newListItem = document.createElement('li')
+        const index = array.indexOf(element)
 
         if (Array.isArray(element)){
-            generateList(newListItem, element)
+            generateList(newListItem, element, ++callCounter)
         } else{
-            newListItem.innerHTML = element
+            newListItem.innerHTML = `${prefix ? `${prefix}.` : ''}${index + 1}`
         }
 
         newItems.push(newListItem)
@@ -20,7 +22,5 @@ function generateList(domElement, array) {
     newList.append(...newItems)
     domElement.append(newList)
 }
-
-
 
 generateList(body, array)
