4ebf97e4b3629b42a2fc3184d2bcb5897f4b6db9
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-04-02 21:09:30 +0300

    homework 29

---
+++ lesson17/17.2/main.js
@@ -0,0 +1,14 @@
+// За допомогою ajax-запиту вивести погоду
+
+// http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19
+
+// q=XXX - місто, для якого показати погоду
+// temp – температура
+// pressure - тиск
+// description – опис
+// humidity – вологість 
+// speed – швидкість вітру
+// deg - напрям у градусах
+// icon - значок, де 10d код іконки
+// http://openweathermap.org/img/w/10d.png
+
