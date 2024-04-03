d1c7b5bd4a84edba117da8bd2c2ed0a047c942fd
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-04-03 12:39:59 +0300

    Homework 30

--- lesson17/17.2/main.js
+++ lesson17/17.2/main.js
@@ -1,14 +1,42 @@
-// За допомогою ajax-запиту вивести погоду
+const main = document.querySelector('.main')
+const buttons = document.querySelector('.header-buttons')
 
-// http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19
+buttons.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const city = event.target.innerText.toUpperCase()
+        getWeather(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`)
+            .then(renderWeather)
+    }
+})
 
-// q=XXX - місто, для якого показати погоду
-// temp – температура
-// pressure - тиск
-// description – опис
-// humidity – вологість 
-// speed – швидкість вітру
-// deg - напрям у градусах
-// icon - значок, де 10d код іконки
-// http://openweathermap.org/img/w/10d.png
+
+async function getWeather(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+}
+
+async function renderWeather(weatherData) {
+    const weatherObj = {
+        name: weatherData.name,
+        temp: weatherData.main.temp,
+        pressure: weatherData.main.pressure,
+        humidity: weatherData.main.humidity,
+        description: weatherData.weather[0].description[0].toUpperCase() + weatherData.weather[0].description.slice(1),
+        icon: weatherData.weather[0].icon,
+        deg: weatherData.wind.deg,
+        speed: weatherData.wind.speed
+    }
+
+    for (let key in weatherObj) {
+        const element = main.querySelector(`[key = "${key}"]`);
+        if (key === 'icon') {
+            element.src = `http://openweathermap.org/img/w/${weatherObj[key]}.png`;
+        } else {
+            element.innerHTML = weatherObj[key];
+        }
+    }
+    
+    main.classList.remove('display-none');
+}
 
