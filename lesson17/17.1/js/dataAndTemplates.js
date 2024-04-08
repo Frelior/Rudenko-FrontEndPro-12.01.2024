c0a1c7af8af3d5bd0cfc99dfca386be34943a4c0
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-04-08 15:43:23 +0300

    single card

--- lesson17/17.1/js/dataAndTemplates.js
+++ lesson17/17.1/js/dataAndTemplates.js
@@ -7,20 +7,58 @@ export async function fetchData(dataObject) {
   });
 }
 
-export function setProductTemplate(productTitle, productPrice) {
+export function setProductTemplate(productObject) {
   const template = `
-  <div class="col">
-  <div class="card">
+  <div class="card" id= ${productObject.id} category=${productObject.category}>
       <div class="card-body">
-      <h5 class="card-title">${productTitle}</h5>
-      <p class="card-text">Price: $${productPrice}</p>
+      <h5 class="card-title">${productObject.title}</h5>
+      <p class="card-text">Price: $${productObject.price}</p>
       </div>
-  </div>
-</div>`
+  </div>`
+  const resultElement  = document.createElement('div');
+  resultElement.classList.add('col');
+  resultElement.innerHTML = template;
+  return resultElement
+}
 
-  return template
+export function setSingleProductTemplate(productObject) {
+  //making an html element from template
+  const template = `
+  <div class="card single-product__card">
+      <button type="button" class="btn-close" aria-label="Close"></button>
+      <img class="card-img-top" src="${productObject.images[0]}" alt="Image" />
+      <hr>
+      <div class="card-body">
+        <h4 class="card-title">${productObject.title}</h4>
+        <p class="card-text">Brand: ${productObject.brand}</p>
+        <p class="card-text">Price: ${productObject.price}$</p>
+        <p class="card-text">Description: ${productObject.description}</p>
+        <button type="button" class="btn btn-primary single-product__buy">Buy</button>
+      </div>
+  </div>`
+  const resultElement  = document.createElement('div');
+  resultElement.classList.add('single-product');
+  resultElement.innerHTML = template;
+
+  // removing element if clicked ouside product card
+  const card = resultElement.querySelector('.card')
+  resultElement.addEventListener('click', (event) => {
+    if (!card.contains(event.target) || event.target.classList.contains('btn-close')) {
+        resultElement.remove()
+    }
+  })
+
+  //processing buy button
+  const buyButton = resultElement.querySelector('.single-product__buy')
+  buyButton.addEventListener('click', () => {
+    alert(`You have successfully purchased ${productObject.title}. Thank you for choosing our store!`)
+    // renderModalWindow(productObject)
+  })
+
+  return resultElement
 }
 
 export function renderTemplate(parentElement, productTemplate) {
-  parentElement.insertAdjacentHTML('beforeend', productTemplate)
+  parentElement.append(productTemplate)
+  return productTemplate
 }
--- lesson17/17.1/js/main.js
+++ lesson17/17.1/js/main.js
@@ -1,10 +1,10 @@
-import {fetchData, setProductTemplate, renderTemplate} from './dataAndTemplates.js';
+import {fetchData, setProductTemplate, renderTemplate, setSingleProductTemplate} from './dataAndTemplates.js';
 
-
+const main = document.querySelector('.main');
 const productsList = document.querySelector('.products-list');
-
-
 const productsData = {};
+
+
 fetchData(productsData)
     .then(() => {
         const categoriesList = document.querySelector('.categories')
@@ -14,19 +14,26 @@ fetchData(productsData)
             }
         })
     })
-    .catch(error => console.log(error))
+.catch(error => console.log(error))
 
 
 function renderProductsByCategory(category) {
-    const targetProducts = productsData[category]
     productsList.innerHTML = ''
-    
+    const targetProducts = productsData[category]
+
     targetProducts.forEach(product => {
-        renderTemplate(productsList, setProductTemplate(product.title, product.price))
+        const productElement = renderTemplate(productsList, setProductTemplate(product))
+
+        productElement.addEventListener('click', (event) => {
+            if (event.target.closest('.card')) {
+                console.log(product);
+                renderSingleProduct(product)
+            }
+        })
     })
-    productsList.addEventListener('click', (event) => {
-        if (event.target.closest('.card')) {
-            console.log('asdasd');
-        }   
-    })
+}
+
+function renderSingleProduct(product) {
+    const singleProduct = renderTemplate(main, setSingleProductTemplate(product))
+    return singleProduct
 }
